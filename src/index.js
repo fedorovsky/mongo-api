b5f5dc67acc4228d941b5903f4a417ab65f4dfb7
Anton Fedorovskyi <a.fedorovskyi@plarium.com>
2018-11-05 17:17:49 +0200

    add prettier

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import PostModel from './models/Post';
 
 const port = process.env.PORT || 8080;
 
-mongoose.connect('mongodb://Fedorovskyi:ZXCvbnmzxc123@ds251223.mlab.com:51223/blog', { useNewUrlParser: true }).then(
-  () => console.log('Connected DB'),
-  err => console.log('Error connected DB'),
-);
+mongoose
+  .connect(
+    'mongodb://Fedorovskyi:ZXCvbnmzxc123@ds251223.mlab.com:51223/blog',
+    { useNewUrlParser: true }
+  )
+  .then(
+    () => console.log('Connected DB'),
+    err => console.log('Error connected DB')
+  );
 
 const app = express();
 
@@ -22,12 +27,12 @@ app.use(bodyParser.json());
  */
 app.get('/posts', (req, res) => {
   PostModel.find().then((err, posts) => {
-    if(err) {
+    if (err) {
       res.status(200).send(err);
     }
     res.status(200).send(posts);
-  })
-})
+  });
+});
 
 /**
  * POST
@@ -38,25 +43,25 @@ app.post('/posts', (req, res) => {
   const post = new PostModel({
     title: data.title,
     text: data.text,
-  })
+  });
 
-  post.save().then((item) => {
-    return res.status(200).send(item)
-  })
-})
+  post.save().then(item => {
+    return res.status(200).send(item);
+  });
+});
 
 /**
  * DELETE
  */
 app.delete('/posts/:id', (req, res) => {
-  PostModel.findOneAndDelete({ _id:  req.params.id}, (err, post) => {
+  PostModel.findOneAndDelete({ _id: req.params.id }, (err, post) => {
     if (post) {
-      return res.status(200).json({ message: 'Ok' })
+      return res.status(200).json({ message: 'Ok' });
     }
     return res.status(500).json({ message: 'Post not found' });
   });
 });
 
-app.listen(port, "0.0.0.0", () => {
-  console.log("Listening on Port 8080");
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port 8080');
 });
--- src/models/Post.js
+++ src/models/Post.js
@@ -1,12 +1,14 @@
 import mongoose, { Schema } from 'mongoose';
 
-const PostSchema = new Schema({
-  title: String,
-  text: String,
-},
-{
-  timestamps: true,
-});
+const PostSchema = new Schema(
+  {
+    title: String,
+    text: String,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Post = mongoose.model('Post', PostSchema);
 
